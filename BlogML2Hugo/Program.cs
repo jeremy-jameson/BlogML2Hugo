c6f02e2ccfd6694bcbb1c56e6daba93d10c9afa4
Jeremy Jameson <jjameson@technologytoolbox.com>
2021-02-13 14:56:17 -0700

    Parse tags from post content
    Example:
    
    <h3>Tags</h3>
    <ul>
      <li><a href="..." rel="tag">My System</a></li>
      <li><a href="..." rel="tag">Toolbox</a></li>
    </ul>

--- BlogML2Hugo/Program.cs
+++ BlogML2Hugo/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Xml;
 using BlogML.Xml;
+using HtmlAgilityPack;
 using ReverseMarkdown;
 
 namespace BlogML2Hugo
@@ -87,8 +88,21 @@ namespace BlogML2Hugo
 
                 var tags = GetTags(blogDoc, post.ID);
 
+                var postHtml = post.Content.UncodedText;
+
+                if (tags.Count == 0)
+                {
+                    var htmlDoc = new HtmlDocument();
+                    htmlDoc.LoadHtml(postHtml);
+
+                    tags = GetTagsFromPostContent(htmlDoc);
+                    RemoveTagsFromPostContent(htmlDoc);
+
+                    postHtml = htmlDoc.DocumentNode.OuterHtml;
+                }
+
                 var header = ComposeBlogHeader(post, categories, tags);
-                var markdown = mdConverter.Convert(post.Content.UncodedText);
+                var markdown = mdConverter.Convert(postHtml);
 
                 Console.WriteLine($"Writing {slug} ({post.Title})");
 
@@ -174,6 +188,63 @@ namespace BlogML2Hugo
             return tags;
         }
 
+        private static List<string> GetTagsFromPostContent(HtmlDocument doc)
+        {
+            // Parse tags from the content of a blog post that contains HTML
+            // similar to the following:
+            //
+            //    <h3>
+            //      Tags</h3>
+            //    <ul>
+            //      <li><a href="..." rel="tag">My System</a></li>
+            //      <li><a href="..." rel="tag">Toolbox</a></li>
+            //    </ul>
+            //
+            // For the example HTML above, a list containing "My System" and
+            // "Toolbox" would be returned.
+            
+            var tagLinks = doc.DocumentNode.SelectNodes(
+                "//h3[normalize-space() = 'Tags']/following-sibling::ul/li/a");
 
+            var tags = new List<string>();
+
+            if (tagLinks != null)
+            {
+                foreach (var tagLink in tagLinks)
+                {
+                    tags.Add(tagLink.InnerText);
+                }
+            }
+
+            return tags;
+        }
+
+        private static void RemoveTagsFromPostContent(HtmlDocument doc)
+        {
+            // Removes blog post content similar to the following:
+            //
+            //    <h3>
+            //      Tags</h3>
+            //    <ul>
+            //      <li><a href="..." rel="tag">My System</a></li>
+            //      <li><a href="..." rel="tag">Toolbox</a></li>
+            //    </ul>
+
+            var tagsList = doc.DocumentNode.SelectSingleNode(
+                "//h3[normalize-space() = 'Tags']/following-sibling::ul");
+
+            if (tagsList != null)
+            {
+                tagsList.Remove();
+            }
+
+            var tagsHeading = doc.DocumentNode.SelectSingleNode(
+                "//h3[normalize-space() = 'Tags']");
+
+            if (tagsHeading != null)
+            {
+                tagsHeading.Remove();
+            }
+        }
     }
 }
